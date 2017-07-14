aafa09d7c690db6a62e40931fe3330cac89974dd
lomaka <i.lomachuk@kitsoft.kiev.ua>
2017-07-14 18:57:36 +0300

    #79

--- central-js/client/app/service/index/index.controller.js
+++ central-js/client/app/service/index/index.controller.js
@@ -97,7 +97,7 @@ angular.module('app')
         }
 
         function updateCatalog(ctlg) {
-            $scope.catalog = ctlg;
+            $scope.catalogAll = ctlg;
             if ($scope.operator == -1) {
                 // временно для старого бизнеса, после реализации тегов - удалить.
                 if ($state.is("index.oldbusiness") || $state.is("index.subcategory") || $rootScope.isOldStyleView) {
@@ -127,7 +127,69 @@ angular.module('app')
             $rootScope.mainSearchView = !!(($state.is('index') || $state.is('index.catalog')) && $scope.data.region);
         }
 
-        $scope.search = function () {
+      function GetPager(totalItems, currentPage, pageSize) {
+        // default to first page
+        currentPage = currentPage || 1;
+
+        // default page size is 10
+        pageSize = pageSize || 10;
+
+        // calculate total pages
+        var totalPages = Math.ceil(totalItems / pageSize);
+
+        var startPage, endPage;
+        if (totalPages <= 10) {
+          // less than 10 total pages so show all
+          startPage = 1;
+          endPage = totalPages;
+        } else {
+          // more than 10 total pages so calculate start and end pages
+          if (currentPage <= 6) {
+            startPage = 1;
+            endPage = 10;
+          } else if (currentPage + 4 >= totalPages) {
+            startPage = totalPages - 9;
+            endPage = totalPages;
+          } else {
+            startPage = currentPage - 5;
+            endPage = currentPage + 4;
+          }
+        }
+
+        // calculate start and end item indexes
+        var startIndex = (currentPage - 1) * pageSize;
+        var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+        // create an array of pages to ng-repeat in the pager control
+        var pages = Array.apply(null, {length: endPage + 1}).map(Number.call, Number);
+        pages.shift();
+        return {
+          totalItems: totalItems,
+          currentPage: currentPage,
+          pageSize: pageSize,
+          totalPages: totalPages,
+          startPage: startPage,
+          endPage: endPage,
+          startIndex: startIndex,
+          endIndex: endIndex,
+          pages: pages
+        };
+      }
+      $scope.pager = {};
+      $scope.setPage = function (page) {
+        if (page < 1 || page > $scope.pager.totalPages) {
+          return;
+        }
+
+        // get pager object from service
+        var arr = $scope.catalogAll.aService;
+        $scope.pager = GetPager(arr.length, page,20);
+        // get current page of items
+        $scope.catalog.aService = arr.slice($scope.pager.startIndex, $scope.pager.endIndex + 1);
+      }
+
+      $scope.search = function () {
+            $rootScope.searchPage = $scope;
             if (sID_Order_RegExp.test($scope.sSearch)) {
                 return null;
             }
@@ -195,6 +257,11 @@ angular.module('app')
 
                 $rootScope.resultsAreLoading = false;
                 getCounts(fullCatalog);
+              if(Array.isArray($scope.catalogAll)){
+                $scope.catalog=$scope.catalogAll
+              }else{
+                $scope.setPage(1);
+              }
             });
         };
 
